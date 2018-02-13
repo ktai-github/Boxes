b41e88ee372c70d0518bb68a34178aaddd1d28bc
Kevin T <kevin.tai.mail@gmail.com>
2018-02-13 12:01:22 -0800

    created box class

---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Boxes
+//
+//  Created by KevinT on 2018-02-13.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      
+  }
+  return 0;
+}
